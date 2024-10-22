9ff25e433a02ad24fc6bb583c78dbc382deae040
rjkafi <kafi2006jk@gmail.com>
2024-10-22 23:20:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-5xl bg-purple-500'>Knowledge react cafe</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
